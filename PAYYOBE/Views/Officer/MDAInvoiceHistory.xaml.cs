fbad906b2af973b1ce75a76096e2e7f4773dcdbf
Adewole Adewale <sundayadewole1997@gmail.com>
2026-06-29 16:05:23 +0100

    Add project files.

---
+++ PAYYOBE/Views/Officer/MDAInvoiceHistory.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PAYYOBE.Views.Officer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MDAInvoiceHistory : ContentPage
+    {
+        public MDAInvoiceHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
